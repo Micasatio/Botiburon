8336c14df64a67645324a3964097d48efe462eb1
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-10-26 16:25:43 -0500

    Bot Tiburón🦈

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,4 +1,36 @@
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+try {
+let qu = args[1] || '360'
+let q = qu + 'p'
+let v = args[0]
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
+const dl_url = await yt.video[q].download()
+const ttl = await yt.title
+const size = await yt.video[q].fileSizeH
+await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${ttl}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+let n2 = lolh.result.link
+let n3 = lolh.result.size
+let n4 = lolh.result.thumbnail
+await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${n}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
+} catch {
+await conn.reply(m.chat, '*ERROR, NO FUE POSIBLE ENVIARLO*', m)}
+}}
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url>`)
+handler.tags = ['downloader']
+handler.command = /^yt(v|mp4)?$/i
+export default handler
+
+
+
+/*import fetch from 'node-fetch'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import db from '../lib/database.js'
 
@@ -46,9 +78,6 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
 handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
-
 handler.exp = 0
-
-
-export default handler
+export default handler*/
 

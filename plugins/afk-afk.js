c5a0368c622f9119105833ccebf1a17f475a3f3e
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-10-26 19:18:05 -0300

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -5,7 +5,7 @@ let handler = async (m, { text, conn }) => {
     user.afk = + new Date
     user.afkReason = text
     m.reply(`
-MeduS.A-bot® | 🕐 *「 AFK v2 」*
+Bot Tiburón🦈 | 🕐 *「 AFK v2 」*
 
 Ahora estás ausente hasta que vuelvas a enviar un nuevo mensaje, cuando te intenten tagear el usuario será notificado de tu ausencia con el motivo.
 
